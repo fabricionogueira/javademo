a5a0cc3d62e87f7ccb205fe571134e5c884e5815
Fabricio Nogueira <fabricio.nogueira@gmail.com>
2022-06-11 22:39:54 -0300

    First commit

---
+++ src/test/java/br/com/fabricions/javademo/JavademoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabricions.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
